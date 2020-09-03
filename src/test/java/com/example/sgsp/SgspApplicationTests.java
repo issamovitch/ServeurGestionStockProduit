2fd4729878fe9ccf41df0a6b0c2e388dbb70329d
Hamed Issam <hamedissam@gmail.com>
2020-09-03 19:57:03 +0100

    first commit

---
+++ src/test/java/com/example/sgsp/SgspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
